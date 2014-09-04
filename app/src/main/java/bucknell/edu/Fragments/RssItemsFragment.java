81217f40fdea0b2976ff3845058538d512f198c1
Boolli <ll024@bucknell.edu>
2014-09-04 02:06:15 -0400

    Add PullToRefreshLayout dependency to build.gradle

--- app/src/main/java/bucknell/edu/Fragments/RssItemsFragment.java
+++ app/src/main/java/bucknell/edu/Fragments/RssItemsFragment.java
@@ -141,6 +141,7 @@ public class RssItemsFragment extends Fragment implements AbsListView.OnItemClic
     * >Communicating with Other Fragments</a> for more information.
     */
     public interface OnRssItemsFragmentInteractionListener {
+        // TODO: Update argument type and name
         public void onRssItemsFragmentInteraction(String title, String content);
     }
 
