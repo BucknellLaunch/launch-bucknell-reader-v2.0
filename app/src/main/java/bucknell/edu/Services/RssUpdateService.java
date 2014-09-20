3439ccbe0568d0c462025eba3ffc3e3b63357b0c
Boolli <ll024@bucknell.edu>
2014-09-20 11:37:37 -0400

    add basic notification when new articles are detected. Need more polishing though. Change min sdk version to 16

--- app/src/main/java/bucknell/edu/Services/RssUpdateService.java
+++ app/src/main/java/bucknell/edu/Services/RssUpdateService.java
@@ -1,5 +1,8 @@
 package bucknell.edu.Services;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
@@ -12,12 +15,16 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.concurrent.CopyOnWriteArrayList;
 
+import bucknell.edu.Activities.MainActivity;
 import bucknell.edu.Data.RssItem;
 import bucknell.edu.Data.RssResource;
+import bucknell.edu.Fragments.RssItemFeedFragment;
 import bucknell.edu.Interfaces.RssListener;
 import bucknell.edu.bucknellreader.R;
 import bucknell.edu.database.RssSQLiteDataSource;
 import bucknell.edu.sync.RssJsonAsyncTask;
+
+import static bucknell.edu.bucknellreader.R.drawable.*;
 
 public class RssUpdateService extends Service implements RssListener{
     private RssSQLiteDataSource rssSQLiteDataSource;
@@ -87,21 +94,43 @@ public class RssUpdateService extends Service implements RssListener{
     private void updateLastUpdateTime() {
         Date date = Calendar.getInstance().getTime();
         long time = date.getTime();
-        SharedPreferences prefs = getApplicationContext().getSharedPreferences(getString(R.string.shared_preferences_file_name), Context.MODE_PRIVATE);
+        SharedPreferences prefs = getApplicationContext().
+                getSharedPreferences(getString(R.string.shared_preferences_file_name), Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = prefs.edit();
         editor.putLong(getString(R.string.shared_preferences_last_update_time), time);
         editor.apply();
     }
 
     private boolean hasNewRssItems () {
-        return false;
+        SharedPreferences prefs = getApplicationContext().
+                getSharedPreferences(getString(R.string.shared_preferences_file_name), Context.MODE_PRIVATE);
+        long localLatestTime = prefs.getLong(getString(R.string.shared_preferences_latest_rss_item_time), 0);
+        long remoteLatestTime = this.rssItems.get(0).getDateInLong();
+        if (remoteLatestTime > localLatestTime) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     private void sendPushNotifications () {
+        Intent intent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this,0,intent,PendingIntent.FLAG_UPDATE_CURRENT);
 
+        Notification.Builder builder = new Notification.Builder(this)
+                .setSmallIcon(R.drawable.ic_launcher)
+                .setContentTitle("New Article from Bucknellian!")
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true)
+                .setContentText("new article");
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(1,builder.build());
     }
 
     private void checkAndSendPushNotifications() {
+        if (this.rssItems == null || this.rssItems.size() == 0)
+            return;
         if (hasNewRssItems()) {
             sendPushNotifications();
         }
